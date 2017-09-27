243830de9886010045fd195a755b7be402d5f99c
Andrew Duthie <andrew@andrewduthie.com>
2017-09-26 21:18:02 -0400

    Compat: Disable wpautop for posts containing blocks

--- lib/register.php
+++ lib/register.php
@@ -308,7 +308,12 @@ function gutenberg_can_edit_post_type( $post_type ) {
 }
 
 /**
- * Determine whether a post has blocks.
+ * Determine whether a post has blocks. This test optimizes for performance
+ * rather than strict accuracy, detecting the pattern of a block but not
+ * validating its structure. For strict accuracy, you should use the block
+ * parser on post content.
+ *
+ * @see gutenberg_parse_blocks()
  *
  * @since 0.5.0
  *
@@ -317,7 +322,24 @@ function gutenberg_can_edit_post_type( $post_type ) {
  */
 function gutenberg_post_has_blocks( $post ) {
 	$post = get_post( $post );
-	return $post && strpos( $post->post_content, '<!-- wp:' ) !== false;
+	return $post && gutenberg_content_has_blocks( $post->post_content );
+}
+
+/**
+ * Determine whether a content string contains blocks. This test optimizes for
+ * performance rather than strict accuracy, detecting the pattern of a block
+ * but not validating its structure. For strict accuracy, you should use the
+ * block parser on post content.
+ *
+ * @see gutenberg_parse_blocks()
+ *
+ * @since 1.2.0
+ *
+ * @param string $content Content to test.
+ * @return bool Whether the content contains blocks.
+ */
+function gutenberg_content_has_blocks( $content ) {
+	return false !== strpos( $content, '<!-- wp:' );
 }
 
 /**
