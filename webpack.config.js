a6721e0e8626a34c02b38c46c06d5a6188b52584
Riad Benguella <benguella@gmail.com>
2017-11-06 08:36:07 +0100

    Metaboxes: Refactor to render inline without iFrame

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,6 @@ const blocksCSSPlugin = new ExtractTextPlugin( {
 	filename: './blocks/build/style.css',
 } );
 
-// CSS loader for styles specific to loading inside meta box iframes.
-const metaBoxIframeCSSPlugin = new ExtractTextPlugin( {
-	filename: './editor/build/meta-box-iframe.css',
-} );
-
 // Configuration for the ExtractTextPlugin.
 const extractConfig = {
 	use: [
@@ -121,17 +116,9 @@ const config = {
 				use: editBlocksCSSPlugin.extract( extractConfig ),
 			},
 			{
-				test: /meta-box-iframe\.scss$/,
-				include: [
-					/editor/,
-				],
-				use: metaBoxIframeCSSPlugin.extract( extractConfig ),
-			},
-			{
 				test: /\.s?css$/,
 				exclude: [
 					/blocks/,
-					/meta-box-iframe/,
 				],
 				use: mainCSSExtractTextPlugin.extract( extractConfig ),
 			},
@@ -143,7 +130,6 @@ const config = {
 		} ),
 		blocksCSSPlugin,
 		editBlocksCSSPlugin,
-		metaBoxIframeCSSPlugin,
 		mainCSSExtractTextPlugin,
 		new webpack.LoaderOptionsPlugin( {
 			minimize: process.env.NODE_ENV === 'production',
